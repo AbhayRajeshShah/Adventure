bfb1c30eadeefdaeff74da74edf4db98e46d3986
AbhayRajeshShah <71131931+AbhayRajeshShah@users.noreply.github.com>
2021-06-04 12:48:06 +0530

    0.0.91
    ammo pic fix

--- js/index.js
+++ js/index.js
@@ -1,41 +1,47 @@
-//canvas
 var canvas;
 var canvasContext;
 canvas=document.getElementById("gameCanvas");
 canvasContext=canvas.getContext("2d");
 var bluePlayer=new playerclass();
+var npc = new bat();
+var zombies = new zombie();
+var shooterZombies=new shooter();
 
 
-
-//frames
 
 
 window.onload=function(){
     loadImages();
 }
 function imageLoadingDoneSoStartGame(){
-   
+   loadlevel(levelOne);
     var framesPerSec=50;
     setInterval(all,1000/framesPerSec);
-    loadlevel(levelOne);
-    
+       
         
 }
 
 function loadlevel(whichLevel){
+    
     trackGrid=whichLevel.slice();
     keys=0;
+    trophies=0;
+    shooterZombies.shot.x=-200;
+    trophieCount();
+    console.log(trophies);
+
+}
+function trophieCount(){
     for(var i=0;i<trackGrid.length;i++){
         if(trackGrid[i]==goal){
             trophies++;
         }
-        console.log(trophies);
+        projectileShooterCollision=true;
+        projectileBatCollision=true;
+        projectileZombieCollision=true;
     }
-
-bluePlayer.playerReset(carPic);
-
-
 }
+
 
 function all(){
     moveAll();
@@ -43,22 +49,37 @@ function all(){
 }
 
 
-
-
 function moveAll(){
-    
+    zombies.checkMove();
+    shooterZombies.checkMove();
     bluePlayer.playerMove();
     bluePlayer.tracksmh();
-}
+    npc.edgeWrap();
+    npc.move(projectileBatCollision);
+    zombies.tracsmh();
+    shooterZombies.tracksmh(bluePlayer,projectileShooterCollision);
+    shooterZombies.move(projectileShooterCollision);
+    zombies.move(projectileZombieCollision);
+    
 
+    
+    
+}
 
 rect(0,0,canvas.width,canvas.height,"black");
 
 function drawAll(){
+shooterZombies.shooterReset();
+ zombies.reset();
+bluePlayer.playerReset();
 trackCreate();
-bluePlayer.playerReset(carPic);
+zombies.draw(projectileZombieCollision); 
+shooterZombies.draw(projectileShooterCollision);
+bluePlayer.check(zombies);
+bluePlayer.check(npc);
+bluePlayer.check(shooterZombies.shot);
 
+
+bluePlayer.draw();
+npc.draw(projectileBatCollision);
 }
-
-
-
