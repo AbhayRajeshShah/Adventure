9d9fa5da162832bc45b7d9a7b7029fa3e60a0cd7
AbhayRajeshShah <71131931+AbhayRajeshShah@users.noreply.github.com>
2021-06-06 16:04:37 +0530

    0.1.2
    Graphics update and support adjacent rooms
    and much more

--- js/image.js
+++ js/image.js
@@ -5,11 +5,25 @@ var wallPic = document.createElement("img");
 var treePic = document.createElement("img");
 var flagPic = document.createElement("img");
 var goalPic = document.createElement("img");
-var otherCarPic = document.createElement("img");
-
-
+var facingLeft = document.createElement("img");
+var heart=document.createElement("img");
+var lavaPic = document.createElement("img");
+var batPic= document.createElement("img");
+var batDownPic= document.createElement("img");
+var batAbovePic= document.createElement("img");
+var projectilePic=document.createElement("img");
+var projectileLeftPic=document.createElement("img");
+var zombieLeftPic=document.createElement("img");
+var zombiePic=document.createElement("img");
+var ammoPic=document.createElement("img");
+var projectileUpPic=document.createElement("img");
+var projectileDownPic=document.createElement("img");
+var shooterZombiePic=document.createElement("img");
+var shooterZombieLeftPic=document.createElement("img");
+var obsidianPic=document.createElement("img");
 
 var picsToLoad = 0; // set automatically based on imageList in loadImages()
+
 
 function countLoadedImagesAndLaunchIfReady() {
 	picsToLoad--;
@@ -34,7 +48,23 @@ function loadImages() {
 		{varName: wallPic, theFile: "world_wall.png"},
 		{varName: treePic, theFile: "world_tree.png"},
 		{varName: goalPic, theFile: "world_goal.png"},
-		{varName: flagPic, theFile: "world_flag.png"}
+		{varName: flagPic, theFile: "world_flag.png"},
+		{varName: facingLeft, theFile: "player1carleft.png"},
+		{varName: heart, theFile: "heart.png"},
+		{varName: lavaPic, theFile: "thorn.png"},
+		{varName: batPic, theFile: "bat.png"},
+		{varName: batDownPic, theFile: "bat-down.png"},
+		{varName: batAbovePic, theFile: "bat-medium.png"},
+		{varName: projectilePic, theFile: "projectile.png"},
+		{varName: projectileLeftPic, theFile: "projectileleft.png"},
+		{varName: projectileUpPic, theFile: "projectileup.png"},
+		{varName: projectileDownPic, theFile: "projectiledown.png"},
+		{varName: zombieLeftPic, theFile: "zombie.png"},
+		{varName: zombiePic, theFile: "zombieright.png"},
+		{varName: ammoPic, theFile: "ammoCount.png"},
+		{varName: shooterZombieLeftPic, theFile: "shooterZombieLeft.png"},
+		{varName: shooterZombiePic, theFile: "shooterZombie.png"},
+		{varName: obsidianPic, theFile: "obsidian.png"}
 		];
 
 	picsToLoad = imageList.length;
@@ -42,4 +72,5 @@ function loadImages() {
 	for(var i=0;i<imageList.length;i++) {
 		beginLoadingImage(imageList[i].varName, imageList[i].theFile);
 	}
+
 }
