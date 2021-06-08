c997d2e5f8ab4a5cd9e2d1cf623ddf0c1a4f1301
AbhayRajeshShah <71131931+AbhayRajeshShah@users.noreply.github.com>
2021-06-08 19:53:59 +0530

    0.1.5

--- js/input.js
+++ js/input.js
@@ -4,6 +4,8 @@ var up_arrow = 38;
 var down_arrow = 40;
 var left_arrow = 37;
 var right_arrow = 39;
+var One=90;
+var Two=88;
 
 var w=87;
 var s=83
@@ -16,7 +18,7 @@ var d=68;
 document.addEventListener("keydown",keyPressed);
 document.addEventListener("keyup",keyRelease);
 
-bluePlayer.controls(up_arrow,down_arrow,right_arrow,left_arrow);
+bluePlayer.controls(up_arrow,down_arrow,right_arrow,left_arrow,One,Two);
 
 function setInput(event,whichPlayer,setTo){
     if(event.keyCode==whichPlayer.down){
@@ -31,15 +33,21 @@ function setInput(event,whichPlayer,setTo){
     if(event.keyCode==whichPlayer.right){
         whichPlayer.rightCheck=setTo;
     } 
+    if(event.keyCode==whichPlayer.def){
+        whichPlayer.defCheck=setTo;
+    } 
+    if(event.keyCode==whichPlayer.fire){
+        whichPlayer.fireCheck=setTo;
+    } 
 }
 
 
 function keyPressed(evt){
-    
+    evt.preventDefault();
     setInput(evt,bluePlayer,true);
 }
 function keyRelease(evt){
-    
+    evt.preventDefault();
     setInput(evt,bluePlayer,false);
 }
 
